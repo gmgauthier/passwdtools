74441d0126821c88e5bb5d53e028e459cca5e7bc
Greg Gauthier <gregory@moo.com>
2019-03-12 15:53:08 +0000

    remove unnecessary code

--- password.py
+++ password.py
@@ -1,5 +1,3 @@
-import hashlib
-import uuid
 from random import randint
 from secrets import choice
 from string import ascii_letters, digits
@@ -39,21 +37,3 @@ class Password:
         return ''.join(
             choice(ascii_letters + digits) for _ in range(randint(mn, mx))
         )
-
-    # @staticmethod
-    # def hash_password(password):
-    #     salt = uuid.uuid4().hex
-    #     return hashlib.sha512(
-    #         salt.encode() + password.encode()).hexdigest() + ':' + salt
-    #
-    # @staticmethod
-    # def check_password(hashed_password, user_password):
-    #     password, salt = hashed_password.split(':')
-    #     return password == hashlib.sha512(
-    #         salt.encode() + user_password.encode()).hexdigest()
-    #
-    # @staticmethod
-    # def dsa_encode(password):
-    #     hash_object = hashlib.new('DSA')
-    #     hash_object.update(password)
-    #     return hash_object.h
